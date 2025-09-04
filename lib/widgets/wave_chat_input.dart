33e77ef5e0fb35579ce28b5f28dd1a7cc29deff5
Arsen Latipov <latipovarsen2006@gmail.com>
2025-09-04 18:11:48 +0300

    [wave-36] WaveInput text selection design fix

--- lib/widgets/wave_chat_input.dart
+++ lib/widgets/wave_chat_input.dart
@@ -102,100 +102,107 @@ class _WaveChatInoutState extends State<WaveChatInput> {
         ? MdColors.defaultTextInputColor
         : MdColors.disabledTextInputColor;
 
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        Expanded(
-          child: Container(
-            margin: const EdgeInsets.only(
-              bottom: 12,
-              left: 20,
-              top: 20,
-            ),
-            constraints: const BoxConstraints(minHeight: 32, maxHeight: 130),
-            child: TextField(
-              focusNode: _focus,
-              controller: _controller,
-              enabled: true,
-              readOnly: !widget.enabled,
-              keyboardType: TextInputType.multiline,
-              maxLines: null,
-              style: TextStyle(
-                color: widget.enabled
-                    ? MdColors.defaultTextInputColor
-                    : MdColors.disabledInputColor,
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
-                fontFamily: 'Play',
-                letterSpacing: 1,
+    return Theme(
+      data: Theme.of(context).copyWith(
+        textSelectionTheme: const TextSelectionThemeData(
+          selectionColor: MdColors.selectionTextInputColor,
+        ),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.only(
+                bottom: 12,
+                left: 20,
+                top: 20,
               ),
-              cursorColor: inputTextColor,
-              decoration: InputDecoration(
-                isCollapsed: true,
-                contentPadding: widget.contentPadding,
-                hoverColor: Colors.transparent,
-                fillColor: Colors.transparent,
-                hintText: widget.hintText,
-                hintStyle: const TextStyle(
-                  color: MdColors.disabledTextInputColor,
+              constraints: const BoxConstraints(minHeight: 32, maxHeight: 130),
+              child: TextField(
+                focusNode: _focus,
+                controller: _controller,
+                enabled: true,
+                readOnly: !widget.enabled,
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                style: TextStyle(
+                  color: widget.enabled
+                      ? MdColors.defaultTextInputColor
+                      : MdColors.disabledInputColor,
                   fontSize: 14,
                   fontWeight: FontWeight.w400,
                   fontFamily: 'Play',
                   letterSpacing: 1,
                 ),
-                enabledBorder: enabledBorder,
-                focusedBorder: focusedBorder,
-                disabledBorder: disabledBorder,
+                cursorColor: inputTextColor,
+                decoration: InputDecoration(
+                  isCollapsed: true,
+                  contentPadding: widget.contentPadding,
+                  hoverColor: Colors.transparent,
+                  fillColor: Colors.transparent,
+                  hintText: widget.hintText,
+                  hintStyle: const TextStyle(
+                    color: MdColors.disabledTextInputColor,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    fontFamily: 'Play',
+                    letterSpacing: 1,
+                  ),
+                  enabledBorder: enabledBorder,
+                  focusedBorder: focusedBorder,
+                  disabledBorder: disabledBorder,
+                ),
               ),
             ),
           ),
-        ),
-        AnimatedContainer(
-          duration: const Duration(milliseconds: 200),
-          curve: Curves.ease,
-          width: _showArrow ? 32 : 0,
-          height: _showArrow ? 32 : 0,
-          margin: const EdgeInsets.only(
-            top: 20,
-            right: 20,
-            left: 12,
-            bottom: 12,
-          ),
-          child: _showArrow
-              ? Container(
-                  clipBehavior: Clip.antiAlias,
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(6),
-                      bottomRight: Radius.circular(12),
-                      topLeft: Radius.circular(6),
-                      topRight: Radius.circular(6),
-                    ),
-                    border: Border.all(
-                      color: borderColor,
-                      width: 2,
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(4),
-                    child: IconButton(
-                      iconSize: 24,
-                      padding: EdgeInsets.zero,
-                      splashColor: Colors.transparent,
-                      highlightColor: Colors.transparent,
-                      hoverColor: Colors.transparent,
-                      icon: SvgPicture.asset(
-                        PrecachedIcons.sendMsgIcon,
-                        colorFilter:
-                            ColorFilter.mode(borderColor, BlendMode.srcIn),
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 200),
+            curve: Curves.ease,
+            width: _showArrow ? 32 : 0,
+            height: _showArrow ? 32 : 0,
+            margin: const EdgeInsets.only(
+              top: 20,
+              right: 20,
+              left: 12,
+              bottom: 12,
+            ),
+            child: _showArrow
+                ? Container(
+                    clipBehavior: Clip.antiAlias,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(6),
+                        bottomRight: Radius.circular(12),
+                        topLeft: Radius.circular(6),
+                        topRight: Radius.circular(6),
                       ),
-                      onPressed: widget.enabled ? _handleSend : null,
+                      border: Border.all(
+                        color: borderColor,
+                        width: 2,
+                      ),
                     ),
-                  ),
-                )
-              : const SizedBox.shrink(),
-        )
-      ],
+                    child: Padding(
+                      padding: const EdgeInsets.all(4),
+                      child: IconButton(
+                        iconSize: 24,
+                        padding: EdgeInsets.zero,
+                        splashColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        icon: SvgPicture.asset(
+                          PrecachedIcons.sendMsgIcon,
+                          colorFilter:
+                              ColorFilter.mode(borderColor, BlendMode.srcIn),
+                        ),
+                        onPressed: widget.enabled ? _handleSend : null,
+                      ),
+                    ),
+                  )
+                : const SizedBox.shrink(),
+          )
+        ],
+      ),
     );
   }
 }
