d0dbfc06cab9e8d1ec9afbafaee428295a07c14b
Arsen Latipov <latipovarsen2006@gmail.com>
2025-09-17 18:10:42 +0300

    [wave-45] WaveTextButtonStory onPressed fix

--- lib/_stories/widgets/wave_text_button_story.dart
+++ lib/_stories/widgets/wave_text_button_story.dart
@@ -18,10 +18,12 @@ class MdTextButtonStory extends StatelessWidget {
       initial: 'test-peer',
     );
 
+    final enabled = knobs.boolean(label: "Enabled");
+
     return Center(
       child: WaveTextButton(
         label: label,
-        onPressed: () => print("WaveTextButton Test"),
+        onPressed: enabled ? () => print("WaveTextButton Test") : null,
       ),
     );
   }
