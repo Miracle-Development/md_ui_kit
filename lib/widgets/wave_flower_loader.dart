c1eb3e72c3e3b3b34a4fdbc4b01e465053b4e747
Arsen Latipov <latipovarsen2006@gmail.com>
2025-09-17 12:04:47 +0300

    [wave-47] WaveFlowerLoader borderRadius fix

--- lib/widgets/wave_flower_loader.dart
+++ lib/widgets/wave_flower_loader.dart
@@ -7,14 +7,18 @@ class WaveFlowerLoader extends StatefulWidget {
     super.key,
     this.breathDuration = const Duration(seconds: 2),
     this.rotateDuration = const Duration(seconds: 6),
-    this.cornerRadius = 50.0,
-    this.minScale = 0.03,
+    this.designCornerRadiusPx = 50.0,
+    this.designSizePx = 376.0,
+    this.minScale = 0.05,
     this.maxScale = 1.0,
   });
 
   final Duration breathDuration;
   final Duration rotateDuration;
-  final double cornerRadius;
+
+  final double designCornerRadiusPx;
+  final double designSizePx;
+
   final double minScale;
   final double maxScale;
 
@@ -42,11 +46,6 @@ class _WaveFlowerLoaderState extends State<WaveFlowerLoader>
     end: 2 * math.pi,
   ).animate(_rotCtrl);
 
-  MorphableShapeBorder _roundedTriangle(double rPx) => PolygonShapeBorder(
-        sides: 3,
-        cornerRadius: Length(rPx),
-      );
-
   @override
   void dispose() {
     _rotCtrl.dispose();
@@ -56,7 +55,7 @@ class _WaveFlowerLoaderState extends State<WaveFlowerLoader>
 
   @override
   Widget build(BuildContext context) {
-    final shape = _roundedTriangle(widget.cornerRadius);
+    final radiusRatio = widget.designCornerRadiusPx / widget.designSizePx;
 
     final angles = <double>[15, -33, -75, -15, -63, -105]
         .map((d) => d * math.pi / 180)
@@ -71,44 +70,52 @@ class _WaveFlowerLoaderState extends State<WaveFlowerLoader>
       const Color.fromRGBO(134, 127, 255, 0.20),
     ];
 
-    Widget buildFlower(double baseSize) {
-      Widget layer(double angle, Color color) => Transform.rotate(
-            angle: angle,
-            child: Container(
-              width: baseSize,
-              height: baseSize,
-              decoration: ShapeDecoration(
-                color: color,
-                shape: shape,
-              ),
-            ),
-          );
-
-      return Stack(
-        alignment: Alignment.center,
-        children: List.generate(6, (i) => layer(angles[i], colors[i])),
-      );
-    }
-
     return LayoutBuilder(
       builder: (context, constraints) {
-        final baseSize = math.min(constraints.maxWidth, constraints.maxHeight);
+        final maxSize = math.min(constraints.maxWidth, constraints.maxHeight);
 
         return AnimatedBuilder(
           animation: Listenable.merge([_breathCtrl, _rotCtrl]),
-          builder: (_, __) => Center(
-            child: Transform.rotate(
-              angle: _angle.value,
-              child: Transform.scale(
-                scale: _scale.value,
+          builder: (_, __) {
+            final effectiveSize = maxSize * _scale.value;
+
+            final cornerRadiusPx = effectiveSize * radiusRatio;
+
+            MorphableShapeBorder roundedTriangle(double rPx) =>
+                PolygonShapeBorder(
+                  sides: 3,
+                  cornerRadius: Length(cornerRadiusPx),
+                );
+
+            final shape = roundedTriangle(cornerRadiusPx);
+
+            Widget layer(double angle, Color color) => Transform.rotate(
+                  angle: angle,
+                  child: Container(
+                    width: effectiveSize,
+                    height: effectiveSize,
+                    decoration: ShapeDecoration(
+                      color: color,
+                      shape: shape,
+                    ),
+                  ),
+                );
+
+            return Center(
+              child: Transform.rotate(
+                angle: _angle.value,
                 child: SizedBox(
-                  width: baseSize,
-                  height: baseSize,
-                  child: buildFlower(baseSize),
+                  width: maxSize,
+                  height: maxSize,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children:
+                        List.generate(6, (i) => layer(angles[i], colors[i])),
+                  ),
                 ),
               ),
-            ),
-          ),
+            );
+          },
         );
       },
     );
