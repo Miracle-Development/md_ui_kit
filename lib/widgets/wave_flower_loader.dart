0cd3453ac7db5bf4ce9229825b55258a361cb3d9
Arsen Latipov <latipovarsen2006@gmail.com>
2025-09-17 15:21:11 +0300

    [wave-47] WaveFlowerLoader design fix

--- lib/widgets/wave_flower_loader.dart
+++ lib/widgets/wave_flower_loader.dart
@@ -1,26 +1,10 @@
 import 'dart:math' as math;
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:morphable_shape/morphable_shape.dart';
 
 class WaveFlowerLoader extends StatefulWidget {
-  const WaveFlowerLoader({
-    super.key,
-    this.breathDuration = const Duration(seconds: 2),
-    this.rotateDuration = const Duration(seconds: 6),
-    this.designCornerRadiusPx = 50.0,
-    this.designSizePx = 376.0,
-    this.minScale = 0.05,
-    this.maxScale = 1.0,
-  });
-
-  final Duration breathDuration;
-  final Duration rotateDuration;
-
-  final double designCornerRadiusPx;
-  final double designSizePx;
-
-  final double minScale;
-  final double maxScale;
+  const WaveFlowerLoader({super.key});
 
   @override
   State<WaveFlowerLoader> createState() => _WaveFlowerLoaderState();
@@ -28,90 +12,99 @@ class WaveFlowerLoader extends StatefulWidget {
 
 class _WaveFlowerLoaderState extends State<WaveFlowerLoader>
     with TickerProviderStateMixin {
-  late final AnimationController _breathCtrl =
-      AnimationController(vsync: this, duration: widget.breathDuration)
-        ..repeat(reverse: true);
+  Duration breathDuration = const Duration(milliseconds: 2200);
+  Duration rotateDuration = const Duration(seconds: 8);
+  double minScale = 0.1;
+  double maxScale = 1.0;
 
-  late final AnimationController _rotCtrl =
-      AnimationController(vsync: this, duration: widget.rotateDuration)
-        ..repeat();
+  double size = 376.0;
+  double radius = 50.0;
+  double closedCornerBoost = 30.0;
+
+  List<double> layerAnglesDeg = const [15, -33, -75, -15, -63, -105];
+  List<double> layerSizeMul = const [1.00, 1.10, 1.24, 1.00, 1.10, 1.24];
+  List<Color> layerColors = const [
+    Color.fromRGBO(220, 218, 255, 0.2),
+    Color.fromRGBO(158, 152, 255, 0.2),
+    Color.fromRGBO(134, 127, 255, 0.2),
+    Color.fromRGBO(220, 218, 255, 0.2),
+    Color.fromRGBO(158, 152, 255, 0.2),
+    Color.fromRGBO(134, 127, 255, 0.2),
+  ];
+  late final _breath =
+      AnimationController(vsync: this, duration: breathDuration)
+        ..repeat(reverse: true);
+  late final _rotate =
+      AnimationController(vsync: this, duration: rotateDuration)..repeat();
 
   late final Animation<double> _scale = Tween<double>(
-    begin: widget.minScale,
-    end: widget.maxScale,
-  ).animate(CurvedAnimation(parent: _breathCtrl, curve: Curves.easeInOut));
+    begin: minScale,
+    end: maxScale,
+  ).animate(CurvedAnimation(parent: _breath, curve: Curves.easeInOut));
 
-  late final Animation<double> _angle = Tween<double>(
-    begin: 0.0,
-    end: 2 * math.pi,
-  ).animate(_rotCtrl);
+  late final Animation<double> _angle =
+      Tween<double>(begin: 0.0, end: 2 * math.pi).animate(_rotate);
 
   @override
   void dispose() {
-    _rotCtrl.dispose();
-    _breathCtrl.dispose();
+    _rotate.dispose();
+    _breath.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final radiusRatio = widget.designCornerRadiusPx / widget.designSizePx;
-
-    final angles = <double>[15, -33, -75, -15, -63, -105]
-        .map((d) => d * math.pi / 180)
-        .toList();
-
-    final colors = <Color>[
-      const Color.fromRGBO(220, 218, 255, 0.20),
-      const Color.fromRGBO(158, 152, 255, 0.20),
-      const Color.fromRGBO(134, 127, 255, 0.20),
-      const Color.fromRGBO(220, 218, 255, 0.20),
-      const Color.fromRGBO(158, 152, 255, 0.20),
-      const Color.fromRGBO(134, 127, 255, 0.20),
-    ];
+    final openCornerRatio = radius / size;
+    final angles = layerAnglesDeg.map((d) => d * math.pi / 180).toList();
 
     return LayoutBuilder(
-      builder: (context, constraints) {
-        final maxSize = math.min(constraints.maxWidth, constraints.maxHeight);
+      builder: (context, c) {
+        final maxSide = math.min(c.maxWidth, c.maxHeight);
 
         return AnimatedBuilder(
-          animation: Listenable.merge([_breathCtrl, _rotCtrl]),
+          animation: Listenable.merge([_breath, _rotate]),
           builder: (_, __) {
-            final effectiveSize = maxSize * _scale.value;
+            final tOpen = ((_scale.value - minScale) / (maxScale - minScale))
+                .clamp(0.0, 1.0);
 
-            final cornerRadiusPx = effectiveSize * radiusRatio;
+            final baseSize = maxSide * _scale.value;
 
-            MorphableShapeBorder roundedTriangle(double rPx) =>
-                PolygonShapeBorder(
-                  sides: 3,
-                  cornerRadius: Length(cornerRadiusPx),
-                );
+            Widget layer(int i) {
+              final color = layerColors[i];
+              final angle = angles[i];
+              final sizeMul = layerSizeMul[i];
+              final size = baseSize * sizeMul;
 
-            final shape = roundedTriangle(cornerRadiusPx);
+              final openCornerPx = size * openCornerRatio;
 
-            Widget layer(double angle, Color color) => Transform.rotate(
-                  angle: angle,
-                  child: Container(
-                    width: effectiveSize,
-                    height: effectiveSize,
-                    decoration: ShapeDecoration(
-                      color: color,
-                      shape: shape,
-                    ),
-                  ),
-                );
+              final targetClosedPx = openCornerPx + closedCornerBoost;
+
+              double cornerPx =
+                  lerpDouble(openCornerPx, targetClosedPx, 1 - tOpen)!;
+
+              cornerPx = cornerPx.clamp(0.0, size * 0.5 - 0.5);
+
+              final shape = PolygonShapeBorder(
+                sides: 3,
+                cornerRadius: Length(cornerPx),
+              );
+
+              return Transform.rotate(
+                angle: angle,
+                child: Container(
+                  width: size,
+                  height: size,
+                  decoration: ShapeDecoration(color: color, shape: shape),
+                ),
+              );
+            }
 
             return Center(
               child: Transform.rotate(
                 angle: _angle.value,
-                child: SizedBox(
-                  width: maxSize,
-                  height: maxSize,
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children:
-                        List.generate(6, (i) => layer(angles[i], colors[i])),
-                  ),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: List.generate(6, layer),
                 ),
               ),
             );
