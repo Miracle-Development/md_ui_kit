4a8cf8e4f625863a35a403ea28b55fd7bf3ba04c
Arsen Latipov <latipovarsen2006@gmail.com>
2025-09-19 15:08:14 +0300

    [wave-48] icon name fix

--- lib/_core/precached_icons.dart
+++ lib/_core/precached_icons.dart
@@ -18,7 +18,8 @@ class PrecachedIcons {
   static const inputOpenedEyeIcon = 'assets/icons/input/opened_eye.svg';
   static const inputClosedEyeIcon = 'assets/icons/input/closed_eye.svg';
   static const micButton = 'assets/icons/mic/microphone_button.svg';
-  static const participantMicIcon = 'assets/icons/participants/pause.svg';
+  static const participantMicIcon =
+      'assets/icons/participants/mic_muted_mini.svg';
 
   List<SvgAssetLoader> get iconsToPrecache => const [
         SvgAssetLoader(copyDefaultIcon),
